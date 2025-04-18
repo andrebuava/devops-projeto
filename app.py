786cba417c9ab7ac52005c1536bd0c4de5f97147
andrebuava <buavaandre@gmail.com>
2025-04-17 23:37:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Olá, mundo! Sua aplicação Flask está rodando no Docker."
 
+@app.route('/boasvindas')
+def boas_vindas():
+    return "Boa tarde! Bem-vindo à aplicação Flask."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
